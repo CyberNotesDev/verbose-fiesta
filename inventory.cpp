4454115e78c44d1baee104b4a0ca343599fdf1cc
Elijah Littell-Sivley <elittell214@gmail.com>
2022-10-13 11:19:29 -0400

    Stored variables

--- inventory.cpp
+++ inventory.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+ unsigned int cola = 4;
+ unsigned int cherryCola = 4;
+ unsigned int sprite = 4;
+ unsigned int dietCola = 4;
+  
+ cout << "We have " << cola << " colas' << endl;
+}
