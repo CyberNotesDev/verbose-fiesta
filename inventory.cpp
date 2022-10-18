98a9e45fc9df09d373e742c4904e36e36d87e144
Elijah Littell-Sivley <elittell214@gmail.com>
2022-10-18 09:26:04 -0400

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void askCustomer() {
+ string customersRequest;
+ string keyWordPop = "pop";
+ cout << "Welcome to Chicago Style Pizza, How may we help you today! << endl;
+  
+ getline(cin, customersRequest);
+ string findKeyWordPop = customersRequest.find(keyWordPop);
+ if (findKeyWordPop != npos) {
+  cout << "Found keyword pop" << endl;
+ }
+}
 
 int main () {
  unsigned int popsBought;
  unsigned int cola = 4, cherryCola = 4, sprite = 4, dietCola = 4;
   
- cout << "We have " << cola << " colas' << endl;
+ askCustomer();
   
  cout << "How many pops would you like? ";
  cin >> popsBought;
