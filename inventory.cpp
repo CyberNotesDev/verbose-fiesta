a91dc3b9446ffa2928ee646979e82185b55a0fc5
Elijah Littell-Sivley <elittell214@gmail.com>
2022-10-20 07:46:32 -0400

    Changed the function

--- inventory.cpp
+++ inventory.cpp
@@ -2,28 +2,24 @@
 #include <string>
 using namespace std;
 
-void askCustomer() {
+int main () {
+ unsigned int popsBought;
+ unsigned int cola = 4, cherryCola = 4, sprite = 4, dietCola = 4;
+  
  string customersRequest;
  string keyWordPop = "pop";
  cout << "Welcome to Chicago Style Pizza, How may we help you today! << endl;
   
+ 
  getline(cin, customersRequest);
  string findKeyWordPop = customersRequest.find(keyWordPop);
  if (findKeyWordPop != npos) {
   cout << "Found keyword pop" << endl;
  }
-}
-
-int main () {
- unsigned int popsBought;
- unsigned int cola = 4, cherryCola = 4, sprite = 4, dietCola = 4;
-  
- askCustomer();
   
  cout << "How many pops would you like? ";
  cin >> popsBought;
  
  unsigned int totalPopsBought = cola - popsBought;
  cola = cola - popsBought;
- 
 }
