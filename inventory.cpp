988297902559229ebd088649a017776e99865f71
Elijah Littell-Sivley <elittell214@gmail.com>
2022-11-16 09:41:40 -0500

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -7,12 +7,14 @@ using namespace std;
 
 int main () {
  // Tax amount in Michigan
- unsigned short double tax = 1.06;
+ unsigned short double salesPrice = 1.06;  
  // Assign variables for the amount we have of that food/drink 
- unsigned int twoLiterOfCola = 8, twoLiterOfCherryCola = 4, twoLiterOfSprite = 4, twoLiterOfDietCola = 4;
- unsigned int twentyOzOfCola = 16, twentyOzOfCherryCola = 4, twentyOzOfLiterOfSprite = 4, twentyOzOfLiterOfDietCola = 8;
- unsigned int tenInchDeep, fourteenInchDeep, sixteenInchDeep;
+ unsigned short int twoLiterOfCola = 8, twoLiterOfCherryCola = 4, twoLiterOfSprite = 4, twoLiterOfDietCola = 4;
+ unsigned short int twentyOzOfCola = 16, twentyOzOfCherryCola = 4, twentyOzOfLiterOfSprite = 4, twentyOzOfLiterOfDietCola = 8;
+ unsigned short int tenInchDeep, fourteenInchDeep, sixteenInchDeep;
  
  unsigned double priceOfTenInchDeep, priceOfFourteenInchDeep, priceOfSixteenInchDeep;
- unsigned double salesPrice = price * tax;
+ // Actual price of food/drinks before tax
+ unsigned short double netSalesPrice = 0.01;
+ unsigned short double grossPrice = netSalesPrice * tax;
 }
