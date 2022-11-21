064637a1311d06c65d811a1834a9cecfba2b6da3
Elijah Littell-Sivley <elittell214@gmail.com>
2022-11-21 14:11:40 +0000

    Got rid of the Welcome function, organized comments, cleaned up .h file

--- inventory.cpp
+++ inventory.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
-// So I can set decimal precision
-#include <iomanip>
-using namespace std;
-// Import my own header
-#include <inventory.h>
+#include <thread> // Uses sleep_for
+#include <chrono> // uses seconds
+#include <iomanip> // So I can set decimal precision
+#include "inventory.h"
 
-int main () {
- welcome();
- myOrder();
- return 0;
+int main() {
+    std::cout << "Welcome to Chicago Style Pizza!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    
+    myOrder();
+
+    return 0;
 }
